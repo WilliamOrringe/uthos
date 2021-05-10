86e4664122d42278a2f6cbc38c2f348d3f7fd3a7
WilliamOrringe <williamorringe@gmail.com>
2021-05-10 17:40:34 +0100

    updates

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,84 +1,33 @@
 <template>
-  <div id="app">
-    <router-view/>
-    <!--Do not change -->
-    <div v-if="this.$route.name !== 'LoginPage' && this.$route.name !== 'RegisterPage' && this.$route.name !==
-    'ForgotPassword' && this.$route.name !== 'EnterNewPassword'"
-         style="margin-bottom: 11vh;">
-      <Navbar/>
-    </div>
-    <Particles id="tsparticles" :options="{
-      fpsLimit: 30,
-      particles: {
-        number: {
-          value: 20
-        },
-        color: {
-          value: '#4CF1C4'
-        },
-        move: {
-          enable: true
-        },
-        size: {
-          random: true,
-          value: 3
-        }
-      }
-    }"
-    />
+  <div id="nav">
+    <router-link to="/">Home</router-link>
+    |
+    <router-link to="/about">About</router-link>
+    |
+    <router-link to="/register">Register</router-link>
   </div>
+  <router-view />
 </template>
 
-<script>
-import Navbar from "./components/Navbar.vue"
-
-export default {
-  name: 'App',
-  components: {
-    Navbar
-  }
-}
-</script>
-
-<style>
-/* Do not change - ensures lower page content is visible above navbar */
-body {
-  min-height: 100vh;
-  height: 100%;
-  overflow:visible;
-  background: rgb(34,25,59);
-  background: linear-gradient(180deg, rgba(34,25,59,1) 0%, rgba(22,16,38,1) 35%, rgba(1,0,1,1) 100%);
-  margin-bottom: 11vh;
-}
+<style lang="scss">
 #app {
-  font-family: 'Trebuchet MS', 'Lucida Sans Unicode', 'Lucida Grande', 'Lucida Sans', Arial, sans-serif;
+  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
-  color: white;
-}
-.navbar-container {
-  margin-top: 200px;
-}
-.navigation-display {
-  color: #7E7E7E;
-  font-size: 12px;
-  position: absolute;
-  top:0;
-  left: 0;
-  padding: 10px;
-}
-#tsparticles {
-  position: fixed;
-  width: 100%;
-  height: 100%;
-  top: 0;
-  left: 0;
-  z-index: -1;
+  color: #316870;
 }
 
-button {
-  cursor: pointer;
-  outline: none;
+#nav {
+  padding: 30px;
+
+  a {
+    font-weight: bold;
+    color: #2c3e50;
+
+    &.router-link-exact-active {
+      color: #42b983;
+    }
+  }
 }
 </style>
